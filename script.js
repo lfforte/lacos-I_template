56f8289ab7c6b711387678d57506c6aad65b18a9
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2023-12-07 20:46:53 -0300

    Solução dos exercícios

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //###-------------------------------Prática 1
 
-/* let soma = 0;
+let soma = 0;
 let aux = 1;
 
-const solicitaNumeros = () =>{
-    while(aux != 0){
-        aux = Number(prompt("Digite um número qualquer ou 0 pra sair:"));
-        soma += aux;
-        //console.log(soma);
-    }
+const solicitaNumeros = () => {
+  while (aux != 0) {
+    aux = Number(prompt("Digite um número qualquer ou 0 pra sair:"));
+    soma += aux;
+    //console.log(soma);
+  }
 }
 
-if (soma === 0){
-    solicitaNumeros();
+if (soma === 0) {
+  solicitaNumeros();
 }
-console.log(soma); */
+console.log(soma);
 
 //###-------------------------------Prática 2
 
@@ -30,11 +30,11 @@ contador(num); */
 
 //###-------------------------------Prática 3
 
-const imprimeArray = () => {
+/* const imprimeArray = () => {
   const arrayNumeros = [0, 1, 2, 3, 4, 5, 6];
   for (let i = 0; i < arrayNumeros.length; i++) {
     console.log(`O elemento do índice ${i} é ${arrayNumeros[i]}`);
   }
 }
 
-imprimeArray();
+imprimeArray(); */
